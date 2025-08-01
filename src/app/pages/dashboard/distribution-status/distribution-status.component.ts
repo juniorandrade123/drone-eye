4d55222d5c4e0e5db07b273dd280aa21f4c876d7
Geraldo Junior <geraldo.junior@asa.com.br>
2025-08-01 10:50:12 -0300

    feat: projeto

---
+++ src/app/pages/dashboard/distribution-status/distribution-status.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-distribution-status',
+  templateUrl: './distribution-status.component.html'
+})
+export class DistributionStatusComponent {}
---
+++ src/app/pages/dashboard/inventories/inventories.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventories',
+  templateUrl: './inventories.component.html'
+})
+export class InventoriesComponent {}
