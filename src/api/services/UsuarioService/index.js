a61dde7a841c410cddceb4824ff747bd62b96eb1
Frederico Rabelo <frederico.rabelo@cwi.com.bro>
2025-08-07 20:29:10 -0300

    crud usuario

--- src/api/services/UsuarioService/index.js
+++ src/api/services/UsuarioService/index.js
@@ -1,11 +1,27 @@
 import { Api } from "../../config";
 import { buscaEmpresaId } from "../../config/auth";
 
-const authEndpoint = 'usuario';
+const authEndpoint = 'usuario/';
 
 export async function getUsers() {
     const empresaId = buscaEmpresaId();
-    const response = await Api.get(authEndpoint + `/?empresa_id=${empresaId}`);
+    const response = await Api.get(authEndpoint + `?empresa_id=${empresaId}`);
 
     return response;
 }
+
+export async function createUser(payload) {
+    const response = await Api.post(authEndpoint , payload);
+    return response;
+}
+
+export async function editUser(id, payload) {
+    const response = await Api.put(authEndpoint + id , payload);
+    return response;
+}
+
+export async function deleteUser(id) {
+    const response = await Api.remove(authEndpoint + id);
+    return response;
+}
+
